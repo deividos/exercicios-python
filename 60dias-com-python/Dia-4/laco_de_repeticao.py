21c34c4374cb402032ce605f276b4750cbbcbab5
deividos <min.deivid@gmail.com>
2026-02-09 22:52:50 -0300

    dia 4

---
+++ 60dias-com-python/Dia-4/laco_de_repeticao.py
@@ -0,0 +1,13 @@
+# Created on iPad.
+
+for i in range (1, 11):
+    print (i)
+
+numero = 0
+
+while numero < 11:
+    print(numero)
+    numero = numero+1
+
+
+    
---
+++ conversor-m-cm-mm.py
@@ -0,0 +1,15 @@
+medida = float(input("Digite uma medida em metros: "))
+medida_cm = medida * 100
+medida_mm = medida * 1000
+
+print ("A medida, ", medida, "m. Em centímetros é: ", medida_cm,"cm. Em milímetros é: ", medida_mm,"mm.")
+# Seu código com melhorias visuais
+#medida = float(input("Digite uma medida em metros: "))
+
+#medida_cm = medida * 100
+#medida_mm = medida * 1000
+
+# Usando f-string e limitando a 2 casas decimais com :.2f
+#print(f"A medida {medida}m corresponde a:")
+#print(f"-> {medida_cm:.2f} cm")
+#print(f"-> {medida_mm:.2f} mm")
